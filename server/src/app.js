6cdc115600197d3457474b1e7aa9f9059bc2dcdf
glnstanley <glnstanley@gmail.com>
2018-09-09 22:24:22 -0400

    add initial server build

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,23 @@
-console.log('hello world!! woohoo')
+console.log('hello world!! woohoo3')
+
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('combined')) // enable logging
+app.use(bodyParser.json()) // json parsing
+app.use(cors()) // enable client access
+
+// get, put, post, push, patch - request types
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
+
+console.log('after listener port 8081: ' + app.log)
